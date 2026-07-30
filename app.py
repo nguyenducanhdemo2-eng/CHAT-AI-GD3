84bf06d1521e8ec14210bbdb2c17707e98dde3c7
NGUYENDUCANHPH65820 <nguyenducanhdemo2@gmail.com>
2026-07-30 13:11:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import google.genai as genai
 app = Flask(__name__)
 
 # ĐIỀN API KEY VÀO ĐÂY HOẶC SỬ DỤNG BIẾN MÔI TRƯỜNG
-API_KEY = os.environ.get("API_KEY", "AQ.Ab8RN6IpQYubQ04IuV11wVCBSI5AVTgLe3bjVy4S0-UfvEA8Gg")
+API_KEY = os.environ.get("API_KEY", "AQ.Ab8RN6LGjFR0sSsPfknRGXfL0Gk3JNMhNxZ_LFhoIaDNjSKy2g")
 if not API_KEY:
     raise ValueError("Thiếu API_KEY. Thiết lập biến môi trường API_KEY hoặc gán trực tiếp vào app.py")
 
